29f3194a1c28b9c31a3eb539a4ea64cd17e2b4fc
Shashgavhale30 <gavhaleshashwati@gmail.com>
2026-08-26 20:02:07 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://your-backend.onrender.com/api',
+  baseURL: 'https://store-rating-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Interceptor to add auth token to requests
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
